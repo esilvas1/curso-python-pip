0301b1519fc74bc75db0cee0c8ad21a0a79fe8b5
esilvas <edwinsils@outlook.com>
2026-01-18 11:36:21 -0500

    Reconfig FastAPI library to create a web server and share page

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,29 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3,4]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Contact Us</title>
+        </head>
+        <body>
+            <h1>Contact Information</h1>
+            <p>Email: contact@example.com</p>
+        </body>
+    </html>
+    """
+
+
+
 
 def run():
     store.get_categories()
